4061a5be39da381ef153b3e3505555e5c021da75
Md Siam Munshi <Tech Support@DESKTOP-1AN7D45>
2026-05-06 15:46:41 +1000

    Add project files.

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Book title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author name is required")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "ISBN is required")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "Availability status is required")]
+        public string AvailabilityStatus { get; set; }
+
+        public string CoverImageUrl { get; set; }
+    }
+}
