31852482ced1393b85f303784a0190a7eb9c65ed
20029397-hub <20029397@students.koi.edu.au>
2026-05-11 03:09:18 +1000

    Add files via upload
    Added Borrowing System implementation

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace LibrarySystem.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<BorrowRecord> BorrowRecords { get; set; }
+    }
+}
---
+++ Models/BorrowRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LibrarySystem.Models
+{
+    public class BorrowRecord
+    {
+        public int Id { get; set; }
+
+        public int MemberId { get; set; }
+        public int BookId { get; set; }
+
+        public DateTime BorrowDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public string Status { get; set; }
+        public decimal FineAmount { get; set; }
+    }
+}
