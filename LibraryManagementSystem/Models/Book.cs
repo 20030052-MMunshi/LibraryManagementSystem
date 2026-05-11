e58141b1d555cffefe8843be36e5ad35e072cc87
Md Siam MUNSHI <20030052@students.koi.edu.au>
2026-05-11 15:57:15 +1000

    Improved Book validation and status dropdown

--- LibraryManagementSystem/Models/Book.cs
+++ LibraryManagementSystem/Models/Book.cs
@@ -7,15 +7,19 @@ namespace LibraryManagementSystem.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Book title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot be more than 100 characters")]
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Author name is required")]
+        [StringLength(80, ErrorMessage = "Author name cannot be more than 80 characters")]
         public string Author { get; set; }
 
         [Required(ErrorMessage = "Genre is required")]
+        [StringLength(50, ErrorMessage = "Genre cannot be more than 50 characters")]
         public string Genre { get; set; }
 
         [Required(ErrorMessage = "ISBN is required")]
+        [StringLength(13, MinimumLength = 10, ErrorMessage = "ISBN must be between 10 and 13 characters")]
         public string ISBN { get; set; }
 
         [Required(ErrorMessage = "Availability status is required")]
